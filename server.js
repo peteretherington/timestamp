9c3cd29cb7d896ac99e2f0f2e4b21d191bb615d4
Peter Etherington <pjg.etherington@gmail.com>
2020-01-06 11:39:12 -0500

    WIP: unix timestamp conversion. Added moment

--- server.js
+++ server.js
@@ -3,12 +3,13 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const moment = require('moment');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -20,19 +21,40 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// Timestamp
 app.get("/api/timestamp/:date_string?", function (req, res) {
   const dateString = req.params.date_string;
-  let date = new Date();
-  if (dateString) date = new Date(dateString);
+  const utc = /[0-9]{4}-[0-9]{2}-[0-9]{2}/;
+  const validUTCString = utc.test(dateString);
+  let date, data;
 
-  res.json({
-    "unix": date.getTime(),
-    "utc": date.toUTCString(),
-  });
+  if (dateString && validUTCString) {
+    console.log('valid UTC string');
+    date = new Date(dateString);
+    data = {
+      "unix": date.getTime(),
+      "utc": date.toUTCString(),
+    };
+  } else if (dateString) {
+    console.log('invalid UTC string');
+    date = moment.unix(dateString).format('ddd, D MMM YYYY HH:mm:ss');
+    data = {
+      "unix": dateString,
+      "utc": date,
+    };
+  } else {
+    console.log('empty Date string');
+    date = new Date();
+    data = {
+      "unix": date.getTime(),
+      "utc": date.toUTCString(),
+    };
+  }
+
+  res.json(data);
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
