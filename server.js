bb3e4aa705e629b5b90332ac7225115540b86dbe
Peter Etherington <pjg.etherington@gmail.com>
2020-01-07 13:39:13 -0500

    Complete getTimeStamp func

--- server.js
+++ server.js
@@ -5,56 +5,48 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const moment = require('moment');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 const cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/views/index.html');
 });
 
+const getTimeStamp = dateString => {
+	let date;
+
+	// If the date string is empty, create a new Date with no argument.
+	if (!dateString) {
+		date = new Date();
+	} else {
+		// If the date string is Not a Number, create a new Date with the date string as the argument.
+		if (isNaN(dateString)) {
+			date = new Date(dateString);
+			// Else create a new Date with the date string converted to an Integer and passed as an argument.
+		} else {
+			date = new Date(parseInt(dateString));
+		}
+	}
+
+	return {
+		unix: date.getTime(),
+		utc: date.toUTCString()
+	};
+};
 
 // Timestamp
-app.get("/api/timestamp/:date_string?", function (req, res) {
-  const dateString = req.params.date_string;
-  const utc = /[0-9]{4}-[0-9]{2}-[0-9]{2}/;
-  const validUTCString = utc.test(dateString);
-  let date, data;
-
-  if (dateString && validUTCString) {
-    console.log('valid UTC string');
-    date = new Date(dateString);
-    data = {
-      "unix": date.getTime(),
-      "utc": date.toUTCString(),
-    };
-  } else if (dateString) {
-    console.log('invalid UTC string');
-    date = moment.unix(dateString).format('ddd, D MMM YYYY HH:mm:ss');
-    data = {
-      "unix": dateString,
-      "utc": date,
-    };
-  } else {
-    console.log('empty Date string');
-    date = new Date();
-    data = {
-      "unix": date.getTime(),
-      "utc": date.toUTCString(),
-    };
-  }
-
-  res.json(data);
-});
+app.get('/api/timestamp/:date_string?', (req, res) =>
+	res.json(getTimeStamp(req.params.date_string))
+);
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT, function() {
+	console.log('Your app is listening on port ' + listener.address().port);
 });
